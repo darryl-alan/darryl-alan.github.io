e7dc1703f87d14bdf25cbbb638d44dbe74b7c311
darryl-alan <drl.iceman@gmail.com>
2019-04-08 13:34:46 +0700

    Continue

--- index.php
+++ index.php
@@ -5,13 +5,20 @@
 <html>
 	<head>
 		<title>seekdarryl.com</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <!--		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>-->
-		<link href="https://unpkg.com/treeflex/dist/css/treeflex.css" rel="stylesheet">
+
 		<style>
 			.full {
 				width:100vw;
 				height: 100vh;
+				max-width:100%;
+				position:relative;
+			}
+
+			.full-width {
+				width:100vw;
 				max-width:100%;
 				position:relative;
 			}
@@ -58,65 +65,69 @@
 				<a href="#about">&#x25BC;</a>
 			</div>
 		</div>
-		<div class="full bg-red" id="about">
-			<div class="center">
-				<img src="lalala.jpg" style="background-color: #fff;width:30%;height:30%;">
-				<h5>Darryl Alan</h5>
-				<div class="tf-tree tf-gap-sm">
-					<ul>
-						<li>
-							<span class="tf-nc">Developer</span>
-							<ul>
-								<li>
-									<span class="tf-nc">Full Stack</span>
-									<ul>
-										<li>
-											<span class="tf-nc">Front End</span>
-											<ul>
-												<li><span class="tf-nc">Bootstrap</span></li>
-												<li><span class="tf-nc">React.js</span></li>
-												<li><span class="tf-nc">jQuery</span></li>
-											</ul>
-										</li>
-										<li>
-											<span class="tf-nc">Back End</span>
-											<ul>
-												<li><span class="tf-nc">PHP</span></li>
-												<li><span class="tf-nc">Node.js</span></li>
-											</ul>
-										</li>
-										<li>
-											<span class="tf-nc">Database</span>
-											<ul>
-												<li><span class="tf-nc">MySQL/MariaDB</span></li>
-												<li><span class="tf-nc">MS SQL Server</span></li>
-												<li><span class="tf-nc">SQLite</span></li>
-<!--												<li><span class="tf-nc">MongoDB</span></li>-->
-											</ul>
-										</li>
-									</ul>
-								</li>
-								<li>
-									<span class="tf-nc">Mobile</span>
-									<ul>
-										<li><span class="tf-nc">Android</span></li>
-										<li><span class="tf-nc">React Native</span></li>
-									</ul>
-								</li>
-								<li>
-									<span class="tf-nc">Deep Learning</span>
-									<ul>
-<!--										<li><span class="tf-nc">Python Numpy</span></li>-->
-										<li><span class="tf-nc">Tensorflow</span></li>
-										<li><span class="tf-nc">Keras</span></li>
-										<li><span class="tf-nc">Pandas</span></li>
-									</ul>
-								</li>
-							</ul>
-						</li>
-					</ul>
+		<div class="full-width bg-red" id="about">
+			<div class="container py-5 text-center">
+				<div class="row">
+					<div class="col-xs-12 col-md-6 pr-md-5 text-md-right">
+						<img src="photo.jpg" style="background-color: #fff;width:250px;max-width:100%;">
+					</div>
+					<div class="col-xs-12 col-md-4 pr-md-5">
+						<h1>Darryl Alan</h1>
+						<p>
+							I'm currently a full-stack and mobile developer at Matrix IT.
+							I have nerdy enthusiasm for all things tech.
+						</p>
+					</div>
 				</div>
-			</div>	
+				<div class="row mt-3">
+					<div class="col-xs-12 col-md-4 mt-3">
+						<div class="card">
+							<div class="card-header">
+								Full Stack
+							</div>
+							<div class="card-body">
+								<ul class="list-group list-group-flush">
+									<li class="list-group-item">CSS3</li>
+									<li class="list-group-item">Bootstrap</li>
+									<li class="list-group-item">JavaScript</li>
+									<li class="list-group-item">jQuery</li>
+									<li class="list-group-item">React</li>
+									<li class="list-group-item">KendoUI</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="col-xs-12 col-md-4 mt-3">
+						<div class="card">
+							<div class="card-header">
+								Mobile
+							</div>
+							<div class="card-body">
+								<ul class="list-group list-group-flush">
+									<li class="list-group-item">Android</li>
+									<li class="list-group-item">React Native</li>
+
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="col-xs-12 col-md-4 mt-3">
+						<div class="card">
+							<div class="card-header">
+								Deep Learning
+							</div>
+							<div class="card-body">
+								<ul class="list-group list-group-flush">
+									<li class="list-group-item">Tensorflow</li>
+									<li class="list-group-item">Keras</li>
+									<li class="list-group-item">Pandas</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+				<a href="#portfolio" style="font-size:30px;">See my work<br>&#x25BC;</a>
+			</div>
 		</div>
 		<div class="full bg-yellow">
 
